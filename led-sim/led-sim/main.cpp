33ac217e139cd572ab864c0e940c3792184a0ede
Mike Emery <memery@yahoo-inc.com>
2016-11-04 10:37:15 -0700

    moving files around

---
+++ led-sim/led-sim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  led-sim
+//
+//  Created by  Mike Emery on 11/4/16.
+//  Copyright © 2016 Mike Emery. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
